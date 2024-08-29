e9cba5be2e113a209afa61a81859244853b08b1f
baohaoliao <liaobaohao@gmail.com>
2024-08-29 20:25:40 +0200

    debug

--- galore_torch/__init__.py
+++ galore_torch/__init__.py
@@ -1,4 +1,4 @@
 from .adafactor import Adafactor as GaLoreAdafactor
 from .adamw import AdamW as GaLoreAdamW
 from .adamw8bit import AdamW8bit as GaLoreAdamW8bit
-from .adamw_ab import AdamW as AdamWLoRA
+from .adamw_lora import AdamW as AdamWLoRA
