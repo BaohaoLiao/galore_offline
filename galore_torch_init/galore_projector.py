ab832f99cf495a00c2b0b74462fe9433bbdb54f2
baohaoliao <liaobaohao@gmail.com>
2024-09-01 01:27:28 +0200

    debug

--- galore_torch_init/galore_projector.py
+++ galore_torch_init/galore_projector.py
@@ -42,7 +42,12 @@ class GaLoreProjector:
                 self.ortho_matrix = self.get_orthogonal_matrix(full_rank_grad, self.rank, type='full')
             low_rank_grad_A = torch.matmul(self.ortho_matrix[0].t(), full_rank_grad)
             low_rank_grad_B = torch.matmul(full_rank_grad, self.ortho_matrix[1].t())
-            return low_rank_grad_A, low_rank_grad_B
+            if full_rank_grad.shape[0] >= full_rank_grad.shape[1]:
+                return low_rank_grad_B
+            else:
+                return low_rank_grad_A
+
+            #return low_rank_grad_A, low_rank_grad_B
         return low_rank_grad
 
     def project_back(self, low_rank_grad):
@@ -64,10 +69,9 @@ class GaLoreProjector:
         elif self.proj_type == 'full':
             #full_rank_grad = torch.matmul(self.ortho_matrix[0], low_rank_grad) @ self.ortho_matrix[1]
             if low_rank_grad[0].shape[0] > low_rank_grad[1].shape[1]:
-                full_rank_grad = torch.matmul(self.ortho_matrix[0], low_rank_grad[0]) 
-                
+                full_rank_grad = torch.matmul(self.ortho_matrix[1], low_rank_grad) 
             else:
-                full_rank_grad = torch.matmul(low_rank_grad[1], self.ortho_matrix[1]) 
+                full_rank_grad = torch.matmul(low_rank_grad, self.ortho_matrix[0]) 
             #full_rank_grad_A = torch.matmul(self.ortho_matrix[0], low_rank_grad[0])
             #full_rank_grad_B = torch.matmul(low_rank_grad[1], self.ortho_matrix[1])
             #full_rank_grad = (full_rank_grad_A + full_rank_grad_B) / 2            
