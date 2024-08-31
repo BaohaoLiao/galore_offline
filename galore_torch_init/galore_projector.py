ad63832031928314e2f769a891bf203a6b453bff
baohaoliao <liaobaohao@gmail.com>
2024-09-01 01:10:10 +0200

    debug

--- galore_torch_init/galore_projector.py
+++ galore_torch_init/galore_projector.py
@@ -63,9 +63,13 @@ class GaLoreProjector:
             full_rank_grad = torch.matmul(self.ortho_matrix, low_rank_grad)
         elif self.proj_type == 'full':
             #full_rank_grad = torch.matmul(self.ortho_matrix[0], low_rank_grad) @ self.ortho_matrix[1]
-            full_rank_grad_A = torch.matmul(self.ortho_matrix[0], low_rank_grad[0])
-            full_rank_grad_B = torch.matmul(low_rank_grad[1], self.ortho_matrix[1])
-            full_rank_grad = (full_rank_grad_A + full_rank_grad_B) / 2            
+            if low_rank_grad[0].shape[0] > low_rank_grad[1].shape[1]:
+                full_rank_grad = torch.matmul(low_rank_grad[1], self.ortho_matrix[1]) 
+            else:
+                full_rank_grad = torch.matmul(self.ortho_matrix[0], low_rank_grad[0]) 
+            #full_rank_grad_A = torch.matmul(self.ortho_matrix[0], low_rank_grad[0])
+            #full_rank_grad_B = torch.matmul(low_rank_grad[1], self.ortho_matrix[1])
+            #full_rank_grad = (full_rank_grad_A + full_rank_grad_B) / 2            
 
         return full_rank_grad * self.scale
 
